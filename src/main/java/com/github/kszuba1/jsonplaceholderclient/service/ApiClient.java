d0cf4b4722acad68f9c0b3990b1a024af688ee5f
Krzysztof Szuba <krzysiu.sz120@gmail.com>
2025-09-23 11:40:14 +0200

    extract http call method - easier to test

--- src/main/java/com/github/kszuba1/jsonplaceholderclient/service/ApiClient.java
+++ src/main/java/com/github/kszuba1/jsonplaceholderclient/service/ApiClient.java
@@ -1,22 +1,15 @@
 package com.github.kszuba1.jsonplaceholderclient.service;
 
 import com.github.kszuba1.jsonplaceholderclient.config.JsonPlaceholderProperties;
-import com.github.kszuba1.jsonplaceholderclient.exception.ApiClientException;
-import com.github.kszuba1.jsonplaceholderclient.exception.ApiServerException;
 import com.github.kszuba1.jsonplaceholderclient.model.Post;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 @RequiredArgsConstructor
@@ -24,40 +17,14 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class ApiClient {
 
-  private static final String HTTP_ERROR_MSG = "%s error when calling: %s";
-
-  private final RestTemplate restTemplate;
   private final JsonPlaceholderProperties properties;
+  private final HttpExecutor httpExecutor;
 
   public List<Post> getPosts() {
     final var headers = new HttpHeaders();
     final var postsPath = "/posts";
-    return makeCall(postsPath, headers, HttpMethod.GET, null, new ParameterizedTypeReference<>(){});
+    final var url = properties.getBaseUri() + postsPath;
+
+    return httpExecutor.execute(url, headers, HttpMethod.GET, null, new ParameterizedTypeReference<>(){});
   }
-
-  private <T> T makeCall(String path, HttpHeaders headers, HttpMethod method, Object body, ParameterizedTypeReference<T> responseType) {
-    final var url = properties.getBaseUri() + path;
-    try {
-      log.info("Sending {} request to {}", method, url);
-
-      final var response = restTemplate.exchange(url, method, new HttpEntity<>(body, headers), responseType);
-
-      log.info("Successfully received response from {}", url);
-
-      return response.getBody();
-    } catch (HttpClientErrorException e) {
-      String errorMsg = String.format(HTTP_ERROR_MSG, e.getStatusCode(), url);
-      log.error(errorMsg, e);
-      throw new ApiClientException(errorMsg, e);
-    } catch (HttpServerErrorException e) {
-      String errorMsg = String.format(HTTP_ERROR_MSG, e.getStatusCode(), url);
-      log.error(errorMsg, e);
-      throw new ApiServerException(errorMsg, e);
-    } catch (RestClientException e) {
-      String errorMsg = String.format(HTTP_ERROR_MSG, "Unexpected", url);
-      log.error(errorMsg, e);
-      throw new ApiClientException(errorMsg, e);
-    }
-  }
-
 }
---
+++ src/main/java/com/github/kszuba1/jsonplaceholderclient/service/HttpExecutor.java
@@ -0,0 +1,50 @@
+package com.github.kszuba1.jsonplaceholderclient.service;
+
+import com.github.kszuba1.jsonplaceholderclient.exception.ApiClientException;
+import com.github.kszuba1.jsonplaceholderclient.exception.ApiServerException;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class HttpExecutor {
+
+  private static final String HTTP_ERROR_MSG = "%s error when calling: %s";
+
+  private final RestTemplate restTemplate;
+
+  public <T> T execute(String url, HttpHeaders headers, HttpMethod method, Object body, ParameterizedTypeReference<T> responseType) {
+    try {
+      log.info("Sending {} request to {}", method, url);
+
+      final var response = restTemplate.exchange(url, method, new HttpEntity<>(body, headers), responseType);
+
+      log.info("Successfully received response from {}", url);
+
+      return response.getBody();
+    } catch (HttpClientErrorException e) {
+      String errorMsg = String.format(HTTP_ERROR_MSG, e.getStatusCode(), url);
+      log.error(errorMsg, e);
+      throw new ApiClientException(errorMsg, e);
+    } catch (HttpServerErrorException e) {
+      String errorMsg = String.format(HTTP_ERROR_MSG, e.getStatusCode(), url);
+      log.error(errorMsg, e);
+      throw new ApiServerException(errorMsg, e);
+    } catch (RestClientException e) {
+      String errorMsg = String.format(HTTP_ERROR_MSG, "Unexpected", url);
+      log.error(errorMsg, e);
+      throw new ApiClientException(errorMsg, e);
+    }
+  }
+}
